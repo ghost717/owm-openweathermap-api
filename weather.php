b5106556ee0170ae6bef38e8a9a47476de70a18a
ghost717 <dawid.zawadzki7717@gmail.com>
2019-10-31 16:51:20 +0100

    getting data

--- weather.php
+++ weather.php
@@ -25,15 +25,30 @@ require './vendor/autoload.php';
 require_once __DIR__ . '/bootstrap.php';
 
 class MyWeather extends OpenWeatherMap {
-    
+
     function __construct($cityNames, $myApiKey) {
         $httpRequestFactory = new RequestFactory();
         $httpClient = GuzzleAdapter::createWithConfig([]);
         $this->owm = new OpenWeatherMap($myApiKey, $httpClient, $httpRequestFactory);
         
         $this->cityNames = explode(",",$cityNames);
+        $this->get_weather();
     }
 
+    function get_weather(){
+        foreach ($this->cityNames as $key => $value) {
+            $weather = $this->owm->getWeather($value, 'metric', 'pl');
+
+            $this->mydata[$key]['name'] = trim($value);
+            $this->mydata[$key]['temp'] = $weather->temperature->getValue();
+            $this->mydata[$key]['humidity'] = $weather->humidity->getValue();
+            $this->mydata[$key]['wind'] = $weather->wind->speed->getValue();
+            $this->mydata[$key]['scores'] = 0;
+            $this->mydata[$key]['time'] = $weather->lastUpdate->format('Y-m-d H:i:s');
+        }
+    }
+
+    
 }
   
 $weather = new MyWeather($_GET['city'], $myApiKey);
